73fca79257b376bfd5ff0a3131d21f503882b2fd
Luiz Anjos <lgsanjos@gmail.com>
2024-04-02 16:26:40 +1100

    replace tab with whitespace (missed file)

--- src/parser/serializer.rs
+++ src/parser/serializer.rs
@@ -5,14 +5,11 @@ use super::diff::{DiffOperation, TaskDiff};
 pub fn serialize(diff: &Vec<TaskDiff>) -> String {
     let mut result = String::new();
 
-    dbg!(diff);
-
     diff.iter()
         .into_group_map_by(|task_diff| task_diff.original_task.clone().unwrap().status)
         .into_iter()
         .sorted_by(|a, b| a.0.cmp(&b.0))
         .for_each(|(status, task_diffs)| {
-            dbg!("hello world group by {}", &status);
             result.push_str(&format!("{}:\n", status));
 
             task_diffs
@@ -21,7 +18,7 @@ pub fn serialize(diff: &Vec<TaskDiff>) -> String {
                 .into_iter()
                 .sorted_by(|a, b| a.0.cmp(&b.0))
                 .for_each(|(project, task_diffs2)| {
-                    result.push_str(&format!("\t{}:\n", project));
+                    result.push_str(&format!("  {}:\n", project));
 
                     task_diffs2.iter().for_each(|task_diff| {
                         let serialized_diff = match task_diff.operation {
@@ -43,25 +40,25 @@ pub fn serialize(diff: &Vec<TaskDiff>) -> String {
 
 fn serialize_remove_diff(task_diff: &TaskDiff) -> String {
     let original_task = task_diff.original_task.as_ref().unwrap();
-    format!("\t\t- {}\t{}\n", original_task.id, original_task.title)
+    format!("    - {}  {}\n", original_task.id, original_task.title)
 }
 
 fn serialize_update_task_fields_diff(task_diff: &TaskDiff) -> String {
     let original_task = task_diff.original_task.as_ref().unwrap();
     let new_task = task_diff.new_task.as_ref().unwrap();
 
-    format!("\t\t~ {}\t{}\n", original_task.id, new_task.title)
+    format!("    ~ {}  {}\n", original_task.id, new_task.title)
 }
 
 fn serialize_new_task_diff(task_diff: &TaskDiff) -> String {
     let original_task = task_diff.original_task.as_ref().unwrap();
 
-    format!("\t\t+ {}\t{}\n", original_task.id, original_task.title)
+    format!("    + {}\n", original_task.title)
 }
 
 fn serialize_do_nothing_diff(task_diff: &TaskDiff) -> String {
     let original_task = task_diff.original_task.as_ref().unwrap();
-    format!("\t\t  {}\t{}\n", original_task.id, original_task.title)
+    format!("      {}  {}\n", original_task.id, original_task.title)
 }
 
 #[cfg(test)]
@@ -94,15 +91,15 @@ fn test_serialize() {
 
     let serialized_diff = serialize(&task_diffs);
     let expected = "DOING:
-\tgroceries:
-\t\t- 3\ttitle3
-\tyat:
-\t\t~ 1\ttitle1 - updated
+  groceries:
+    - 3  title3
+  yat:
+    ~ 1  title1 - updated
 TODO:
-\tgroceries:
-\t\t+ 4\ttitle4
-\tyat:
-\t\t  2\ttitle2
+  groceries:
+    + title4
+  yat:
+      2  title2
 ";
     assert_eq!(serialized_diff, expected);
 }
